36d6e70da5028388575dc13e13f62ec8929f0cd9
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-16 14:25:30 +1000

    Added times implementation

---
+++ examples/times.js
@@ -0,0 +1,9 @@
+var times = require('../times');
+
+times(3).forEach(function() {
+  console.log('hello world');
+});
+
+// --> hello world
+// --> hello world
+// --> hello world
---
+++ times.js
@@ -0,0 +1,10 @@
+/**
+  ## times
+
+  Create an element of arrays that can be iterated over n times:
+
+  <<< examples/times.js
+
+**/
+
+module.exports = require('./range')(1);
