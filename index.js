ec6abe41444c2ce78ce5c17ab862799efc9b5cb8
Damon Oehlman <damon.oehlman@gmail.com>
2018-01-16 11:19:42 +1100

    additional exports in the index file (more ES6 / typescript friendly)

--- index.js
+++ index.js
@@ -6,7 +6,17 @@
 
 **/
 
+exports.chain = require('./chain');
+exports.equality = require('./equality');
 exports.flatten = require('./flatten');
+exports.length = require('./length');
+exports.match = require('./match');
 exports.not = require('./not');
+exports.nub = require('./nub');
+exports.nubBy = require('./nub-by');
 exports.pluck = require('./pluck');
+exports.range = require('./range');
+exports.sum = require('./sum');
+exports.times = require('./times');
+exports.within = require('./within');
 exports.zip = require('./zip');
